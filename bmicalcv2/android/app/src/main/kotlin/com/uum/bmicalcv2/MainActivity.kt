2612d97fceebc717cdaa4119b36c9640385d41a2
Foo Roon Yi <janiceyi02@gmail.com>
2022-04-25 22:17:02 +0800

    Revert "Initial commit"
    This reverts commit 4e56bc997ae16b3c885f21bffb44b8b0a5570b62.

--- bmicalcv2/android/app/src/main/kotlin/com/uum/bmicalcv2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.uum.bmicalcv2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- weatherapp/lib/weather.dart
+++
@@ -1,9 +0,0 @@
-class Weather {
-  var loc = "Not available",
-      temp = 0.0,
-      hum = 0,
-      weather = "Not available",
-      feelsLike = 0.0;
-
-  Weather(this.loc, this.temp, this.hum, this.weather, this.feelsLike);
-}
